c8b466c3e3507cc960d0df1bb245a6331e05a46c
Keosha Young <kyoung1012@gmail.com>
2023-01-02 16:50:21 -0500

    Removed User ID from Gym table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_190826) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_214855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_02_190826) do
   create_table "gyms", force: :cascade do |t|
     t.string "address"
     t.string "phone_number"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
